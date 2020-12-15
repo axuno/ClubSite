099ff8bba7308c867d04eddd9dcfb0111459f6cf
Norbert Bietsch <nb@axuno.net>
2020-12-15 20:08:12 +0100

    Introduced HtmlTool.IsEmptyHtml() for Excerpts

---
+++ ClubSite/Library/HtmlTool.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ClubSite.Library
+{
+    public class HtmlTool
+    {
+        /// <summary>
+        /// Checks, whether the input string leaves neither a text literal nor an image tag, after removing all HTML tags and non-breaking spaces except the image tag.
+        /// </summary>
+        /// <param name="input">The input HTML to check.</param>
+        /// <returns>Returns <see langword="true"/>, if the input string neither leaves a text literal nor an image tag, after removing all HTML tags and non-breaking spaces except the image tag, else <see langword="false"/>.</returns>
+        public static bool IsEmptyHtml(string input)
+        {
+            return string.IsNullOrWhiteSpace(Regex.Replace(input, "(<([^>]+)>|&nbsp;)", string.Empty, RegexOptions.Multiline)) 
+                   && !input.Contains("<img", StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
