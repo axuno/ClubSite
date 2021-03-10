9dd3368cdc8c76890affa5ecbda1a031ea79c2bd
Norbert Bietsch <nb@axuno.net>
2021-03-10 18:09:31 +0100

    Added global.json

--- ClubSite/publish-to-web.cmd
+++ ClubSite/publish-to-web.cmd
@@ -1,4 +1,5 @@
 @ECHO OFF
+dotnet --version
 dotnet publish -c release -o ..\..\Web\ /p:EnvironmentName=Production
 REM dotnet publish -c release -r win81-x86 -o ..\Web\
 REM Without -r parameter: Framework-dependent deployment (FDD), else Self-contained deployment (SCD)
