f61f0274049e9e0f680046b4d37987997230bc44
axunonb <nb@axuno.net>
2022-12-23 11:27:44 +0100

    Create new "Content-Feat." block for background color support
    * Draft as v3.4.0
    * Allows to select a website theme color as background color of an HTML content block (HtmlFeaturedBlock)
    * Added ThemeColor Enum
    * Adjusted blocks.scss for better support of small devices
    * Replaced "article > header" wtih "article > div.alert"  in partials

---
+++ ClubSite/Models/HtmlFeaturedBlock.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (C) axuno gGmbH and Contributors.
+// This software may be modified and distributed under the terms
+// of the MIT license. See the LICENSE file for details.
+// https://github.com/axuno/ClubSite
+
+using Piranha.Extend;
+using Piranha.Extend.Blocks;
+using Piranha.Extend.Fields;
+using Piranha.Models;
+
+namespace ClubSite.Models;
+
+[BlockType(Name = "Content-Feat.", Category = "Content", Icon = "fas fa-paragraph", Width = EditorWidth.Centered)]
+public class HtmlFeaturedBlock : HtmlBlock
+{
+    [Field(Title = "Background Color", Description = "Select website theme color")]
+    public SelectField<ThemeColor> BackgroundColor { get; set; } = new(); // titles for select items taken from enum Display attribute
+}
---
+++ ClubSite/Models/ThemeColor.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (C) axuno gGmbH and Contributors.
+// This software may be modified and distributed under the terms
+// of the MIT license. See the LICENSE file for details.
+// https://github.com/axuno/ClubSite
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClubSite.Models;
+
+public enum ThemeColor
+{
+    [Display(Description = "Standard")]
+    Default,
+    [Display(Description = "Hell")]
+    Light,
+    [Display(Description = "Dunkel")]
+    Dark,
+    [Display(Description = "Primär")]
+    Primary,
+    [Display(Description = "Sekundär")]
+    Secondary,
+    [Display(Description = "Erfolg")]
+    Success,
+    [Display(Description = "Gefahr")]
+    Danger,
+    [Display(Description = "Warnung")]
+    Warning,
+    [Display(Description = "Info")]
+    Info
+}
