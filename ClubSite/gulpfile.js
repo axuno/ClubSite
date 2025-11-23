057e09800b9caf95256dbdd4b25f91c64a72e90d
axunonb <axunonb@users.noreply.github.com>
2025-11-23 23:08:10 +0100

    build: Modernize node build process and replace outdated css dependencies
    * Replaced `gulp-cssmin` with `gulp-postcss` and `cssnano` for CSS processing.
    * Updated `autoprefixer` to `10.4.22`, `postcss` to `8.5.6`, and `cssnano` to `7.1.2`.
    * Upgraded `bootstrap` to `5.3.8` for the latest fixes.
    * Removed deprecated dependencies like `gulp-cssmin` and redundant configurations.
    * Introduced `sassOptions` for better Sass compilation configuration, including `includePaths`, `quietDeps`, and `outputStyle`.

--- ClubSite/gulpfile.js
+++ ClubSite/gulpfile.js
@@ -1,45 +1,56 @@
-var gulp = require("gulp"),
-    sass = require('gulp-sass')(require('sass'));
-cssmin = require("gulp-cssmin");
-rename = require("gulp-rename");
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const rename = require('gulp-rename');
+const path = require('path');
 
-gulp.task("min",
+// Configure sass options: resolve imports from node_modules and silence dependency deprecation warnings
+const sassOptions = {
+    // allow @use/@import to resolve packages without relative paths
+    loadPaths: [path.join(__dirname, 'node_modules')], // absolute path to node_modules is more reliable
+    quietDeps: true,                // suppress deprecation warnings coming from dependency packages (Dart Sass option)
+    outputStyle: 'expanded'         // alternative: 'compressed' (we handle minification separately)
+};
+
+gulp.task('min',
     function(done) {
-        gulp.src("assets/scss/style.scss")
-            .pipe(sass().on("error", sass.logError))
-            .pipe(cssmin())
+        gulp.src('assets/scss/style.scss')
+            .pipe(sass(sassOptions).on('error', sass.logError))
+            .pipe(postcss([autoprefixer(), cssnano()]))
             .pipe(rename({
-                suffix: ".min"
+                suffix: '.min'
             }))
-            .pipe(gulp.dest("wwwroot/assets/css"));
-        gulp.src("assets/scss/bootstrap-custom.scss")
-            .pipe(sass().on("error", sass.logError))
-            .pipe(cssmin())
+            .pipe(gulp.dest('wwwroot/assets/css'));
+        gulp.src('assets/scss/bootstrap-custom.scss')
+            .pipe(sass(sassOptions).on('error', sass.logError))
+            .pipe(postcss([autoprefixer(), cssnano()])) // autoprefix + minify
             .pipe(rename({
-                basename: "bootstrap",
-                suffix: ".min"
+                basename: 'bootstrap',
+                suffix: '.min'
             }))
-            .pipe(gulp.dest("wwwroot/assets/css"));
-        gulp.src("assets/scss/fontawesome-custom.scss")
-            .pipe(sass().on("error", sass.logError))
-            .pipe(cssmin())
+            .pipe(gulp.dest('wwwroot/assets/css'));
+        gulp.src('assets/scss/fontawesome-custom.scss')
+            .pipe(sass(sassOptions).on('error', sass.logError))
+            .pipe(postcss([autoprefixer(), cssnano()])) // autoprefix + minify
             .pipe(rename({
-                basename: "fontawesome",
-                suffix: ".min"
+                basename: 'fontawesome',
+                suffix: '.min'
             }))
-            .pipe(gulp.dest("wwwroot/assets/css/fontawesome"));
-        gulp.src("node_modules/@fortawesome/fontawesome-free/webfonts/*")
-            .pipe(gulp.dest("wwwroot/assets/css/fontawesome/webfonts"));
-        gulp.src("assets/fonts/fonts-lato-raleway.css")
-            .pipe(gulp.dest("wwwroot/assets/fonts/"));
-        gulp.src("assets/fonts/lato/*.woff2")
-            .pipe(gulp.dest("wwwroot/assets/fonts/lato/"));  
-        gulp.src("assets/fonts/raleway/*.woff2")
-            .pipe(gulp.dest("wwwroot/assets/fonts/raleway/"));
-        gulp.src("node_modules/bootstrap/dist/js/bootstrap.bundle.min.js")
-            .pipe(gulp.dest("wwwroot/assets/js/bootstrap/"));
+            .pipe(gulp.dest('wwwroot/assets/css/fontawesome'));
+        gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+            .pipe(gulp.dest('wwwroot/assets/css/fontawesome/webfonts'));
+        gulp.src('assets/fonts/fonts-lato-raleway.css')
+            .pipe(gulp.dest('wwwroot/assets/fonts/'));
+        gulp.src('assets/fonts/lato/*.woff2')
+            .pipe(gulp.dest('wwwroot/assets/fonts/lato/'));  
+        gulp.src('assets/fonts/raleway/*.woff2')
+            .pipe(gulp.dest('wwwroot/assets/fonts/raleway/'));
+        gulp.src('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')
+            .pipe(gulp.dest('wwwroot/assets/js/bootstrap/'));
         done();
     });
 
-gulp.task("serve", gulp.parallel(["min"]));
-gulp.task("default", gulp.series("serve"));
+gulp.task('serve', gulp.parallel(['min']));
+gulp.task('default', gulp.series('serve'));
